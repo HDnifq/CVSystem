37eff79a1035545ebdb5d22576025187a479e13e
daixian <amano_tooko@qq.com>
2019-04-19 11:31:10 +0800

    增加调整了一下json的Serialize方法

--- CVSystem/DTO/Serialize.h
+++ CVSystem/DTO/Serialize.h
@@ -31,6 +31,314 @@ namespace json {
 class Serialize
 {
   public:
+#pragma region std_array
+
+    template <size_t _Size>
+    static inline void AddMember(rapidjson::ValueW& value,
+                                 const rapidjson::GenericStringRef<wchar_t>& fieldName,
+                                 const std::array<std::wstring, _Size>& obj, rapidjson::MemoryPoolAllocator<>& allocator)
+    {
+        using namespace rapidjson;
+        ValueW jv(kArrayType);
+        for (size_t i = 0; i < obj.size(); i++) {
+            ValueW item; //string的话必须这样搞一下然后拷贝,它这个库分两种思路,ref的和拷贝的
+            item.SetString(obj[i].c_str(), allocator);
+            jv.PushBack(item, allocator);
+        }
+        value.AddMember(fieldName, jv, allocator);
+    }
+
+    template <typename T, size_t _Size>
+    static inline void AddMember(rapidjson::ValueW& value,
+                                 const rapidjson::GenericStringRef<wchar_t>& fieldName,
+                                 const std::array<T, _Size>& arr, rapidjson::MemoryPoolAllocator<>& allocator)
+    {
+        using namespace rapidjson;
+        ValueW data(kArrayType);
+        for (size_t i = 0; i < arr.size(); i++)
+            data.PushBack(arr[i], allocator);
+        value.AddMember(fieldName, data, allocator);
+    }
+
+    //->json 整个对象是纯array
+    template <typename T, size_t _Size>
+    static inline rapidjson::DocumentW ArrayDoc(const std::array<T, _Size>& arr)
+    {
+        rapidjson::DocumentW doc;
+        doc.SetArray();
+        auto& allocator = doc.GetAllocator();
+        for (size_t i = 0; i < arr.size(); i++)
+            doc.PushBack(arr[i], allocator);
+        return doc;
+    }
+
+    //->obj 不带返回值的,参数作为结果的GetObj
+    template <size_t _Size>
+    static inline void GetObj(const rapidjson::ValueW& value, std::array<double, _Size>& obj)
+    {
+        if (!value.IsArray()) //它应该是一个array
+            return;
+        for (unsigned int i = 0; i < value.Size(); i++) {
+            obj[i] = value[i].GetDouble();
+        }
+    }
+
+    //->obj 不带返回值的,参数作为结果的GetObj
+    template <size_t _Size>
+    static inline void GetObj(const rapidjson::ValueW& value, std::array<float, _Size>& obj)
+    {
+        if (!value.IsArray()) //它应该是一个array
+            return;
+        for (unsigned int i = 0; i < value.Size(); i++) {
+            obj[i] = value[i].GetFloat();
+        }
+    }
+
+    //->obj 不带返回值的,参数作为结果的GetObj
+    template <size_t _Size>
+    static inline void GetObj(const rapidjson::ValueW& value, std::array<int, _Size>& obj)
+    {
+        if (!value.IsArray()) //它应该是一个array
+            return;
+        for (unsigned int i = 0; i < value.Size(); i++) {
+            obj[i] = value[i].GetInt();
+        }
+    }
+
+    //->obj 不带返回值的,参数作为结果的GetObj
+    template <size_t _Size>
+    static inline void GetObj(const rapidjson::ValueW& value, std::array<unsigned int, _Size>& obj)
+    {
+        if (!value.IsArray()) //它应该是一个array
+            return;
+        for (unsigned int i = 0; i < value.Size(); i++) {
+            obj[i] = value[i].GetUint();
+        }
+    }
+
+    //->obj 不带返回值的,参数作为结果的GetObj
+    template <size_t _Size>
+    static inline void GetObj(const rapidjson::ValueW& value, std::array<bool, _Size>& obj)
+    {
+        if (!value.IsArray()) //它应该是一个array
+            return;
+        for (unsigned int i = 0; i < value.Size(); i++) {
+            obj[i] = value[i].GetBool();
+        }
+    }
+
+    //->obj 不带返回值的,参数作为结果的GetObj
+    template <size_t _Size>
+    static inline void GetObj(const rapidjson::ValueW& value, std::array<std::wstring, _Size>& obj)
+    {
+        if (!value.IsArray()) //它应该是一个array
+            return;
+        for (unsigned int i = 0; i < value.Size(); i++) {
+            obj[i] = value[i].GetString();
+        }
+    }
+
+#pragma endregion
+
+#pragma region vector
+
+    static inline void AddMember(rapidjson::ValueW& value,
+                                 const rapidjson::GenericStringRef<wchar_t>& fieldName,
+                                 const std::vector<std::wstring>& obj, rapidjson::MemoryPoolAllocator<>& allocator)
+    {
+        using namespace rapidjson;
+        ValueW jv(kArrayType);
+        for (size_t i = 0; i < obj.size(); i++) {
+            ValueW item; //string的话必须这样搞一下然后拷贝,它这个库分两种思路,ref的和拷贝的
+            item.SetString(obj[i].c_str(), allocator);
+            jv.PushBack(item, allocator);
+        }
+        value.AddMember(fieldName, jv, allocator);
+    }
+
+    template <typename T>
+    static inline void AddMember(rapidjson::ValueW& value,
+                                 const rapidjson::GenericStringRef<wchar_t>& fieldName,
+                                 const std::vector<T>& obj, rapidjson::MemoryPoolAllocator<>& allocator)
+    {
+        using namespace rapidjson;
+        ValueW jv(kArrayType);
+        for (size_t i = 0; i < obj.size(); i++) {
+            jv.PushBack(obj[i], allocator);
+        }
+        value.AddMember(fieldName, jv, allocator);
+    }
+
+    //->json 整个对象是纯array
+    static inline rapidjson::DocumentW ArrayDoc(const std::vector<std::wstring>& obj)
+    {
+        rapidjson::DocumentW doc;
+        doc.SetArray();
+        auto& allocator = doc.GetAllocator();
+        for (size_t i = 0; i < obj.size(); i++) {
+            rapidjson::ValueW item;
+            item.SetString(obj[i].c_str(), allocator);
+            doc.PushBack(item, allocator);
+        }
+        return doc;
+    }
+
+    //obj -> json 整个对象是纯array
+    template <typename T>
+    static inline rapidjson::DocumentW ArrayDoc(const std::vector<T>& obj)
+    {
+        rapidjson::DocumentW doc;
+        doc.SetArray();
+        auto& allocator = doc.GetAllocator();
+        for (size_t i = 0; i < obj.size(); i++) {
+            rapidjson::ValueW item;
+            doc.PushBack(obj[i], allocator);
+        }
+        return doc;
+    }
+
+    //->obj 不带返回值的,参数作为结果的GetObj
+    static inline void GetObj(const rapidjson::ValueW& value, std::vector<std::wstring>& obj)
+    {
+        obj.clear();
+        for (unsigned int i = 0; i < value.Size(); i++) {
+            obj.push_back(value[i].GetString());
+        }
+    }
+
+    //->obj 不带返回值的,参数作为结果的GetObj
+    static inline void GetObj(const rapidjson::ValueW& value, std::vector<double>& obj)
+    {
+        obj.clear();
+        for (unsigned int i = 0; i < value.Size(); i++) {
+            obj.push_back(value[i].GetDouble());
+        }
+    }
+
+    //->obj 不带返回值的,参数作为结果的GetObj
+    static inline void GetObj(const rapidjson::ValueW& value, std::vector<float>& obj)
+    {
+        obj.clear();
+        for (unsigned int i = 0; i < value.Size(); i++) {
+            obj.push_back(value[i].GetFloat());
+        }
+    }
+
+    //->obj 不带返回值的,参数作为结果的GetObj
+    static inline void GetObj(const rapidjson::ValueW& value, std::vector<int>& obj)
+    {
+        obj.clear();
+        for (unsigned int i = 0; i < value.Size(); i++) {
+            obj.push_back(value[i].GetInt());
+        }
+    }
+
+    //->obj 不带返回值的,参数作为结果的GetObj
+    static inline void GetObj(const rapidjson::ValueW& value, std::vector<bool>& obj)
+    {
+        obj.clear();
+        for (unsigned int i = 0; i < value.Size(); i++) {
+            obj.push_back(value[i].GetBool());
+        }
+    }
+
+#pragma endregion
+
+#pragma region string
+
+    static inline void AddMember(rapidjson::ValueW& value, const rapidjson::GenericStringRef<wchar_t>& name, const std::wstring& str, rapidjson::MemoryPoolAllocator<>& allocator)
+    {
+        using namespace rapidjson;
+        ValueW valStr;
+        valStr.SetString(str.c_str(), allocator); //有val的方式是拷贝一次string的方式 ,
+        value.AddMember(name, valStr, allocator);
+
+        //不拷贝的方式是使用StringRef
+        // bid.AddMember("adm", rapidjson::StringRef(html_snippet.c_str(),html_snippet.size()), allocator);
+    }
+
+    static inline void AddMember(rapidjson::ValueW& value, const rapidjson::GenericStringRef<wchar_t>& name, const std::string& str, rapidjson::MemoryPoolAllocator<>& allocator)
+    {
+        using namespace rapidjson;
+        std::wstring wstr = JsonHelper::utf8To16(str);
+        ValueW valStr;
+        valStr.SetString(wstr.c_str(), allocator);
+        value.AddMember(name, valStr, allocator);
+    }
+
+#pragma endregion
+
+#pragma region Vector3
+
+    static inline void AddMember(rapidjson::ValueW& value,
+                                 const rapidjson::GenericStringRef<wchar_t>& fieldName,
+                                 const dxlib::dto::Vector3& obj, rapidjson::MemoryPoolAllocator<>& allocator)
+    {
+        using namespace rapidjson;
+        ValueW jv(kObjectType);
+        jv.AddMember(L"x", obj.x, allocator);
+        jv.AddMember(L"y", obj.y, allocator);
+        jv.AddMember(L"z", obj.z, allocator);
+        value.AddMember(fieldName, jv, allocator);
+    }
+
+    //->obj 不带返回值的,参数作为结果的GetObj
+    static inline void GetObj(const rapidjson::ValueW& value, dxlib::dto::Vector3& obj)
+    {
+        obj.x = value[L"x"].GetFloat();
+        obj.y = value[L"y"].GetFloat();
+        obj.z = value[L"z"].GetFloat();
+    }
+
+    //->obj 带返回值的直接GetObj
+    static inline dxlib::dto::Vector3 GetDtoVector3(const rapidjson::ValueW& value)
+    {
+        dxlib::dto::Vector3 obj;
+        obj.x = value[L"x"].GetFloat();
+        obj.y = value[L"y"].GetFloat();
+        obj.z = value[L"z"].GetFloat();
+        return obj;
+    }
+
+#pragma endregion
+
+#pragma region Quaternion
+
+    static inline void AddMember(rapidjson::ValueW& value,
+                                 const rapidjson::GenericStringRef<wchar_t>& fieldName,
+                                 const dxlib::dto::Quaternion& obj, rapidjson::MemoryPoolAllocator<>& allocator)
+    {
+        using namespace rapidjson;
+        ValueW jv(kObjectType);
+        jv.AddMember(L"x", obj.x, allocator);
+        jv.AddMember(L"y", obj.y, allocator);
+        jv.AddMember(L"z", obj.z, allocator);
+        jv.AddMember(L"w", obj.w, allocator);
+        value.AddMember(fieldName, jv, allocator);
+    }
+
+    //->obj 不带返回值的,参数作为结果的GetObj
+    static inline void GetObj(const rapidjson::ValueW& value, dxlib::dto::Quaternion& obj)
+    {
+        obj.x = value[L"x"].GetFloat();
+        obj.y = value[L"y"].GetFloat();
+        obj.z = value[L"z"].GetFloat();
+        obj.w = value[L"w"].GetFloat();
+    }
+
+    //->obj 带返回值的直接GetObj
+    static inline dxlib::dto::Quaternion GetDtoQuaternion(const rapidjson::ValueW& value)
+    {
+        dxlib::dto::Quaternion obj;
+        obj.x = value[L"x"].GetFloat();
+        obj.y = value[L"y"].GetFloat();
+        obj.z = value[L"z"].GetFloat();
+        obj.w = value[L"w"].GetFloat();
+        return obj;
+    }
+
+#pragma endregion
+
 #pragma region cvMat
 
     //->json 整个对象是一个object
@@ -140,234 +448,6 @@ class Serialize
             for (rapidjson::SizeType i = 0; i < data.Size(); i++) // 使用 SizeType 而不是 size_t
                 _r[i] = data[i].GetDouble();
         }
-    }
-
-#pragma endregion
-
-#pragma region std_array
-
-    template <size_t _Size>
-    static inline void AddMember(rapidjson::ValueW& value,
-                                 const rapidjson::GenericStringRef<wchar_t>& fieldName,
-                                 const std::array<double, _Size>& arr, rapidjson::MemoryPoolAllocator<>& allocator)
-    {
-        using namespace rapidjson;
-        ValueW data(kArrayType);
-        for (size_t i = 0; i < arr.size(); i++)
-            data.PushBack(arr[i], allocator);
-        value.AddMember(fieldName, data, allocator);
-    }
-
-    //->json 整个对象是纯array
-    template <size_t _Size>
-    static inline rapidjson::DocumentW ArrayDoc(const std::array<double, _Size>& arr)
-    {
-        rapidjson::DocumentW doc;
-        doc.SetArray();
-        auto& allocator = doc.GetAllocator();
-        for (size_t i = 0; i < arr.size(); i++)
-            doc.PushBack(arr[i], allocator);
-        return doc;
-    }
-
-    //->obj 不带返回值的,参数作为结果的GetObj
-    template <size_t _Size>
-    static inline void GetObj(const rapidjson::ValueW& value, std::array<double, _Size>& obj)
-    {
-        if (!value.IsArray()) //它应该是一个array
-            return;
-        for (unsigned int i = 0; i < value.Size(); i++) {
-            obj[i] = value[i].GetDouble();
-        }
-    }
-
-#pragma endregion
-
-#pragma region Vector3
-
-    static inline void AddMember(rapidjson::ValueW& value,
-                                 const rapidjson::GenericStringRef<wchar_t>& fieldName,
-                                 const dxlib::dto::Vector3& obj, rapidjson::MemoryPoolAllocator<>& allocator)
-    {
-        using namespace rapidjson;
-        ValueW jv(kObjectType);
-        jv.AddMember(L"x", obj.x, allocator);
-        jv.AddMember(L"y", obj.y, allocator);
-        jv.AddMember(L"z", obj.z, allocator);
-        value.AddMember(fieldName, jv, allocator);
-    }
-
-    //->obj 不带返回值的,参数作为结果的GetObj
-    static inline void GetObj(const rapidjson::ValueW& value, dxlib::dto::Vector3& obj)
-    {
-        obj.x = value[L"x"].GetFloat();
-        obj.y = value[L"y"].GetFloat();
-        obj.z = value[L"z"].GetFloat();
-    }
-
-    //->obj 带返回值的直接GetObj
-    static inline dxlib::dto::Vector3 GetDtoVector3(const rapidjson::ValueW& value)
-    {
-        dxlib::dto::Vector3 obj;
-        obj.x = value[L"x"].GetFloat();
-        obj.y = value[L"y"].GetFloat();
-        obj.z = value[L"z"].GetFloat();
-        return obj;
-    }
-
-#pragma endregion
-
-#pragma region Quaternion
-
-    static inline void AddMember(rapidjson::ValueW& value,
-                                 const rapidjson::GenericStringRef<wchar_t>& fieldName,
-                                 const dxlib::dto::Quaternion& obj, rapidjson::MemoryPoolAllocator<>& allocator)
-    {
-        using namespace rapidjson;
-        ValueW jv(kObjectType);
-        jv.AddMember(L"x", obj.x, allocator);
-        jv.AddMember(L"y", obj.y, allocator);
-        jv.AddMember(L"z", obj.z, allocator);
-        jv.AddMember(L"w", obj.w, allocator);
-        value.AddMember(fieldName, jv, allocator);
-    }
-
-    //->obj 不带返回值的,参数作为结果的GetObj
-    static inline void GetObj(const rapidjson::ValueW& value, dxlib::dto::Quaternion& obj)
-    {
-        obj.x = value[L"x"].GetFloat();
-        obj.y = value[L"y"].GetFloat();
-        obj.z = value[L"z"].GetFloat();
-        obj.w = value[L"w"].GetFloat();
-    }
-
-    //->obj 带返回值的直接GetObj
-    static inline dxlib::dto::Quaternion GetDtoQuaternion(const rapidjson::ValueW& value)
-    {
-        dxlib::dto::Quaternion obj;
-        obj.x = value[L"x"].GetFloat();
-        obj.y = value[L"y"].GetFloat();
-        obj.z = value[L"z"].GetFloat();
-        obj.w = value[L"w"].GetFloat();
-        return obj;
-    }
-
-#pragma endregion
-
-#pragma region vector
-
-    static inline void AddMember(rapidjson::ValueW& value,
-                                 const rapidjson::GenericStringRef<wchar_t>& fieldName,
-                                 const std::vector<std::wstring>& obj, rapidjson::MemoryPoolAllocator<>& allocator)
-    {
-        using namespace rapidjson;
-        ValueW jv(kArrayType);
-        for (size_t i = 0; i < obj.size(); i++) {
-            ValueW item; //string的话必须这样搞一下然后拷贝,它这个库分两种思路,ref的和拷贝的
-            item.SetString(obj[i].c_str(), allocator);
-            jv.PushBack(item, allocator);
-        }
-        value.AddMember(fieldName, jv, allocator);
-    }
-
-    static inline void AddMember(rapidjson::ValueW& value,
-                                 const rapidjson::GenericStringRef<wchar_t>& fieldName,
-                                 const std::vector<int>& obj, rapidjson::MemoryPoolAllocator<>& allocator)
-    {
-        using namespace rapidjson;
-        ValueW jv(kArrayType);
-        for (size_t i = 0; i < obj.size(); i++) {
-            jv.PushBack(obj[i], allocator);
-        }
-        value.AddMember(fieldName, jv, allocator);
-    }
-
-    //->json 整个对象是纯array
-    static inline rapidjson::DocumentW ArrayDoc(const std::vector<std::wstring>& obj)
-    {
-        rapidjson::DocumentW doc;
-        doc.SetArray();
-        auto& allocator = doc.GetAllocator();
-        for (size_t i = 0; i < obj.size(); i++) {
-            rapidjson::ValueW item;
-            item.SetString(obj[i].c_str(), allocator);
-            doc.PushBack(item, allocator);
-        }
-        return doc;
-    }
-
-    //obj -> json 整个对象是纯array
-    template <typename T>
-    static inline rapidjson::DocumentW ArrayDoc(const std::vector<T>& obj)
-    {
-        rapidjson::DocumentW doc;
-        doc.SetArray();
-        auto& allocator = doc.GetAllocator();
-        for (size_t i = 0; i < obj.size(); i++) {
-            rapidjson::ValueW item;
-            doc.PushBack(obj[i], allocator);
-        }
-        return doc;
-    }
-
-    //->obj 不带返回值的,参数作为结果的GetObj
-    static inline void GetObj(const rapidjson::ValueW& value, std::vector<std::wstring>& obj)
-    {
-        obj.clear();
-        for (unsigned int i = 0; i < value.Size(); i++) {
-            obj.push_back(value[i].GetString());
-        }
-    }
-
-    //->obj 不带返回值的,参数作为结果的GetObj
-    static inline void GetObj(const rapidjson::ValueW& value, std::vector<double>& obj)
-    {
-        obj.clear();
-        for (unsigned int i = 0; i < value.Size(); i++) {
-            obj.push_back(value[i].GetDouble());
-        }
-    }
-
-    //->obj 不带返回值的,参数作为结果的GetObj
-    static inline void GetObj(const rapidjson::ValueW& value, std::vector<float>& obj)
-    {
-        obj.clear();
-        for (unsigned int i = 0; i < value.Size(); i++) {
-            obj.push_back(value[i].GetFloat());
-        }
-    }
-
-    //->obj 不带返回值的,参数作为结果的GetObj
-    static inline void GetObj(const rapidjson::ValueW& value, std::vector<int>& obj)
-    {
-        obj.clear();
-        for (unsigned int i = 0; i < value.Size(); i++) {
-            obj.push_back(value[i].GetInt());
-        }
-    }
-
-#pragma endregion
-
-#pragma region string
-
-    static inline void AddMember(rapidjson::ValueW& value, const rapidjson::GenericStringRef<wchar_t>& name, const std::wstring& str, rapidjson::MemoryPoolAllocator<>& allocator)
-    {
-        using namespace rapidjson;
-        ValueW valStr;
-        valStr.SetString(str.c_str(), allocator); //有val的方式是拷贝一次string的方式 ,
-        value.AddMember(name, valStr, allocator);
-
-        //不拷贝的方式是使用StringRef
-        // bid.AddMember("adm", rapidjson::StringRef(html_snippet.c_str(),html_snippet.size()), allocator);
-    }
-
-    static inline void AddMember(rapidjson::ValueW& value, const rapidjson::GenericStringRef<wchar_t>& name, const std::string& str, rapidjson::MemoryPoolAllocator<>& allocator)
-    {
-        using namespace rapidjson;
-        std::wstring wstr = JsonHelper::utf8To16(str);
-        ValueW valStr;
-        valStr.SetString(wstr.c_str(), allocator);
-        value.AddMember(name, valStr, allocator);
     }
 
 #pragma endregion
