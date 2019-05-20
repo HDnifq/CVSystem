1544b4add82211a8faef7f07d5fd8832aa8c6af1
daixian <amano_tooko@qq.com>
2019-05-20 21:36:10 +0800

    尝试增加线程池

---
+++ CVSystem/Common/ThreadPool.cpp
@@ -0,0 +1,15 @@
+﻿#include "ThreadPool.h"
+
+namespace dxlib {
+
+ThreadPool* ThreadPool::m_pInstance = new ThreadPool();
+
+ThreadPool::ThreadPool()
+{
+}
+
+ThreadPool::~ThreadPool()
+{
+}
+
+} // namespace dxlib
---
+++ CVSystem/Common/ThreadPool.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include <boost/asio/thread_pool.hpp>
+#include <boost/asio/post.hpp>
+
+namespace dxlib {
+
+///-------------------------------------------------------------------------------------------------
+/// <summary> 全局的线程池. </summary>
+///
+/// <remarks> Dx, 2019/5/20. </remarks>
+///-------------------------------------------------------------------------------------------------
+class ThreadPool
+{
+  private:
+    /// <summary> The instance. </summary>
+    static ThreadPool* m_pInstance;
+
+  public:
+    ThreadPool();
+    ~ThreadPool();
+
+    /// <summary> 单例. </summary>
+    static ThreadPool* GetInst()
+    {
+        return m_pInstance;
+    }
+
+    void test()
+    {
+        boost::asio::post(pool, [] {});
+    }
+
+  private:
+    boost::asio::thread_pool pool{4}; // 4 threads
+};
+} // namespace dxlib
