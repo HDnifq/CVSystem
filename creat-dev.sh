475f17e1369d66e7a76e77526c6230764427942c
daixian <amano_tooko@qq.com>
2019-12-24 14:45:57 +0800

    修正了一个头文件缺失通过了gcc编译

--- creat-dev.sh
+++ creat-dev.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#! /bin/bash
 chcp 65001
 echo "生成一个开发用的VS工程目录"
 python3 -V
@@ -7,4 +7,3 @@ python3 ./tools/DownloadLib.py -d"$HOME/dxlib/download" -l"$HOME/dxlib/lib" -p"l
 mkdir build
 cd build
 conan install .. -s build_type=Release --build missing
-# cmake ../src -G "Visual Studio 15 Win64" -DCVSYSTEM_BUILD_SHARED:BOOL="0" -DCVSYSTEM_BUILD_TESTS:BOOL="1" -DCMAKE_CONFIGURATION_TYPES:STRING="Release;RelWithDebInfo"
--- src/CVSystem/Proc/Camera.h
+++ src/CVSystem/Proc/Camera.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
 #include <opencv2/opencv.hpp>
+#include <memory>
 
 namespace dxlib {
 
