fbbf0b2b8d9a092ff047e3fef64ce02cf309d00d
daixian <amano_tooko@qq.com>
2020-04-29 18:19:30 +0800

    linux下编译

--- creat-dev.sh
+++ creat-dev.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-chcp 65001
+# chcp 65001
 echo "生成一个开发用的VS工程目录"
 python3 -V
-python3 ./tools/DownloadLib.py -d"$HOME/dxlib/download" -l"$HOME/dxlib/lib" -p"linux"  "concurrentqueue"
+# python3 ./tools/DownloadLib.py -d"$HOME/dxlib/download" -l"$HOME/dxlib/lib" -p"linux"  "concurrentqueue"
 
 mkdir build
 cd build
