fb8b8b1a08fc6f46499e8179fac9ea35c157d435
daixian <amano_tooko@qq.com>
2020-11-04 11:23:24 +0800

    加个处理算法

--- src/CamCapture/main.cpp
+++ src/CamCapture/main.cpp
@@ -16,16 +16,72 @@ public:
 
     void process(pCameraImage camImage, int& key) override
     {
-        cv::Mat image = camImage->vImage[0].image;
+        for (size_t i = 0; i < camImage->vImage.size(); i++)
+        {
+            if (camImage->vImage[i].isSuccess == false)
+            {
+                LogE("TestProc.process():有个相机采图失败!");
+                continue;
+            }
+            cv::Mat image = camImage->vImage[i].image;
+            cv::Mat gray; cv::Mat thr40; cv::Mat  thr110; cv::Mat thr180;
+            thresholdOnce(image, gray, thr40, thr110, thr180);
+        }
 
         count++;                     //采图的计数加1
 
-        try {
-            key = cv::waitKey(1); //一定要加waitKey无法显示图片
-        }
-        catch (const std::exception& e) {
+        //try {
+        //    key = cv::waitKey(1); //一定要加waitKey无法显示图片
+        //}
+        //catch (const std::exception& e) {
+        //}
+    }
+
+    /**
+     * 为了节省开销,一步生成灰色图和门限图.
+     *
+     * @author daixian
+     * @date 2020/3/30
+     *
+     * @param [in,out] image  The image.
+     * @param [in,out] gray   The gray.
+     * @param [in,out] thr40  The thr 40.
+     * @param [in,out] thr110 The thr 110.
+     * @param [in,out] thr180 The thr 180.
+     */
+    void thresholdOnce(cv::Mat& image, cv::Mat& gray, cv::Mat& thr40, cv::Mat& thr110, cv::Mat& thr180)
+    {
+        gray = cv::Mat(image.rows, image.cols, CV_8UC1);
+        thr40 = cv::Mat(image.rows, image.cols, CV_8UC1, cv::Scalar(0, 0, 0));
+        thr110 = cv::Mat(image.rows, image.cols, CV_8UC1, cv::Scalar(0, 0, 0));
+        thr180 = cv::Mat(image.rows, image.cols, CV_8UC1, cv::Scalar(0, 0, 0));
+
+        int nrow = image.rows;
+        int ncol = image.cols;
+        int channels = image.channels();
+        for (int i = 0; i < nrow; i++) {
+            uchar* dataIn = image.ptr<uchar>(i); //get the address of row i;
+            uchar* dataGray = gray.ptr<uchar>(i);
+            uchar* dataThr40 = thr40.ptr<uchar>(i);
+            uchar* dataThr110 = thr110.ptr<uchar>(i);
+            uchar* dataThr180 = thr180.ptr<uchar>(i);
+
+            for (int j = 0; j < ncol; j++) {
+                dataGray[j] = dataIn[j * channels]; //灰色图
+
+                if (dataIn[j * channels] >= 40) {
+                    dataThr40[j] = 255;
+                    if (dataIn[j * channels] >= 80) {
+                        dataThr110[j] = 255;
+                        if (dataIn[j * channels] >= 160) {
+                            dataThr180[j] = 255;
+                        }
+                    }
+                }
+            }
         }
     }
+
     void onEnable() override
     {
     }
@@ -69,7 +125,7 @@ int main(int argc, char* argv[])
 #if defined(_WIN32) || defined(_WIN64)
         kvp.second->setProp(cv::CAP_PROP_EXPOSURE, -11);
 #else
-        kvp.second->setProp(cv::CAP_PROP_EXPOSURE, 10);
+        kvp.second->setProp(cv::CAP_PROP_EXPOSURE, 1);
 #endif
 
         //输出一下
