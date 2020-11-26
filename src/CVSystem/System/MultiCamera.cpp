da1a3a0e9dc50484b120825ab0e2738700339a7a
daixian <amano_tooko@qq.com>
2020-11-26 22:23:24 +0800

    最后试一下休息10ms的cpu情况

--- src/CVSystem/System/MultiCamera.cpp
+++ src/CVSystem/System/MultiCamera.cpp
@@ -97,7 +97,7 @@ class FrameProcRunnable : public Poco::Runnable
                         //LogD("MultiCamera.run():队列等待...");
                         //for (size_t i = 0; i < 100; i++) {
                         //std::this_thread::sleep_for(std::chrono::seconds(1));
-                        std::this_thread::sleep_for(std::chrono::milliseconds(2));
+                        std::this_thread::sleep_for(std::chrono::milliseconds(10));
                         //std::this_thread::yield();
                         //}
                     }
